ee304106199052a3200734845799c9b52372096e
manschko <manschko1999@googlemail.com>
2025-01-25 03:50:08 +0100

    feat: Item Spawn von Bubble und Item Dragging

--- Assets/Code/Item.cs
+++ Assets/Code/Item.cs
@@ -1,8 +1,60 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
-[System.Serializable]
-public class Item
+[Serializable]
+public class Item : MonoBehaviour
 {
     public string Name;
     public Sprite DisplaySprite;
+
+
+    private Rigidbody2D rb;
+    private Camera mainCamera;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        mainCamera = Camera.main;
+    }
+
+    [Range(0.0f, 100.0f)] public float damping = 1.0f;
+
+    [Range(0.0f, 100.0f)] public float frequency = 5.0f;
+
+    public bool debug = true;
+    public Color debugColor = Color.cyan;
+
+    private TargetJoint2D joint;
+
+    void OnMouseDown()
+    {
+        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+
+        // Add a target joint to the Rigidbody2D GameObject.
+        joint = rb.gameObject.AddComponent<TargetJoint2D>();
+        joint.dampingRatio = damping;
+        joint.frequency = frequency;
+
+        // Attach the anchor to the local-point where we clicked.
+        joint.anchor = joint.transform.InverseTransformPoint(mousePosition);
+    }
+
+    void OnMouseUp()
+    {
+        Destroy(joint);
+        joint = null;
+    }
+
+    void OnMouseDrag()
+    {
+        if (!joint)
+            return;
+
+        var worldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        joint.target = worldPos;
+
+        if (debug)
+            Debug.DrawLine(joint.transform.TransformPoint(joint.anchor), worldPos, debugColor);
+    }
 }
--- Assets/Code/ItemManager.cs
+++ Assets/Code/ItemManager.cs
@@ -1,28 +1,25 @@
-
 using UnityEngine;
-public class ItemManager: MonoBehaviour
-{
 
+public class ItemManager : MonoBehaviour
+{
     private void Start()
     {
-        Pot.OnBubbleSpawned.AddListener(bubble =>
-        {
-            bubble.OnPopped.AddListener(() =>
-            {
-                spawnItem(bubble.ContainedItem);
-            });
-        });
+        Pot.OnBubbleSpawned.AddListener(bubble => { bubble.OnPopped.AddListener(() => { spawnItem(bubble); }); });
     }
+
     private void Update()
     {
-        
     }
 
-    private void spawnItem(Item item)
+    private void spawnItem(Bubble bubble)
     {
-        GameObject spawendItem = Instantiate(item.gameObject);
-        
-        item.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(Random.Range(1,15), Random.Range(1,15));
-    }
+        GameObject spawendItem = Instantiate(
+            bubble.ContainedItem.gameObject,
+            bubble.transform.position, 
+            Quaternion.identity
+            );
 
+        spawendItem.GetComponent<Rigidbody2D>().linearVelocity =
+            new Vector2(Random.Range(-15, 15), Random.Range(-15, 15));
+    }
 }
