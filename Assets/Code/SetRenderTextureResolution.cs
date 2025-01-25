d9964641d3dd0a9a74f5cdee4972c290afba9d9a
bear <>
2025-01-25 13:54:28 +0100

    awdawdawdawd

---
+++ Assets/Code/SetRenderTextureResolution.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class SetRenderTextureResolution : MonoBehaviour
+{
+    public Camera Target;
+    public RenderTexture RenderTexture;
+    public float Multiplier = 0.5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        RenderTexture.Release();
+        RenderTexture.width = (int)(Screen.width * Multiplier);
+        RenderTexture.height = (int)(Screen.height * Multiplier);
+       // Target.targetTexture
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
