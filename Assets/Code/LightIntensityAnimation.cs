c65751b901e576105f0b8b9be31c2105a5570860
bear <>
2025-01-25 16:47:01 +0100

    akwuhdakluwhakwuhd

---
+++ Assets/Code/LightIntensityAnimation.cs
@@ -0,0 +1,29 @@
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using Random = UnityEngine.Random;
+
+public class LightIntensityAnimation : MonoBehaviour
+{
+    public Vector2 IntensityRange = new Vector2(0.8f, 1.2f);
+    public float ChangeSpeed = 1f;
+    public Light2D Light;
+
+    private float _pos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _pos = Random.Range(0f, 100f);
+        if(Light == null)
+            Light = GetComponent<Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _pos += Time.deltaTime * ChangeSpeed;
+        var strength = math.remap(0f, 1f, IntensityRange.x, IntensityRange.y, Mathf.PerlinNoise1D(_pos));
+        Light.intensity = strength;
+    }
+}
--- Assets/Code/PopHelper.cs
+++ Assets/Code/PopHelper.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 [DefaultExecutionOrder(50)]
 public class PopHelper : MonoBehaviour
@@ -11,15 +13,44 @@ public class PopHelper : MonoBehaviour
     }
 
     public static float MultiDelay;
+
     public float PerPopDelay = 0.1f;
+
+    private List<Bubble> _bubbles = new List<Bubble>();
+    private bool _didHit;
 
     private void LateUpdate()
     {
         MultiDelay = 0f;
+
+        if(_didHit == false && _bubbles.Count > 0)
+        {
+            HandleMiss();
+        }
+
+        _bubbles.Clear();
+        _didHit = false;
     }
 
     public static void RegisterPop()
     {
         MultiDelay += _instance.PerPopDelay;
+    }
+
+    public static void RegisterNonClick(Bubble bubble)
+    {
+        _instance._bubbles.Add(bubble);
+    }
+
+    public static void RegisterClick()
+    {
+        _instance._didHit = true;
+    }
+
+    private void HandleMiss()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var pos = ray.origin;
+        //_bubbles.Select(x => x.)
     }
 }
